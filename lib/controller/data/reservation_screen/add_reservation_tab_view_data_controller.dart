caea0d7ffc5dd471a2e866ac09d94a93f24c4e94
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-09-25 23:15:39 +0530

    reformatting file

--- lib/controller/data/reservation_screen/add_reservation_tab_view_data_controller.dart
+++ lib/controller/data/reservation_screen/add_reservation_tab_view_data_controller.dart
@@ -1,11 +1,13 @@
 import 'package:get/get.dart';
 import 'package:restaurant_manager/constants/table_constants.dart';
+import 'package:restaurant_manager/controller/data/reservation_screen/reservation_data_controller_mixin.dart';
 import 'package:restaurant_manager/controller/network/reservation_network_controller.dart';
 import 'package:restaurant_manager/exception/network_exception.dart';
 import 'package:restaurant_manager/exception/null_map_key_exception.dart';
+import 'package:restaurant_manager/model/reservation.dart';
 import 'package:restaurant_manager/model/restaurant_table.dart';
 
-class AddReservationTabViewDataController extends GetxController {
+class AddReservationTabViewDataController extends GetxController with ReservationDataControllerMixin{
   static final AddReservationTabViewDataController instance = Get.find();
 
   final ReservationNetworkController _rnc =
@@ -16,14 +18,15 @@ class AddReservationTabViewDataController extends GetxController {
   static Future<AddReservationTabViewDataController> create() async {
     final AddReservationTabViewDataController controller =
         AddReservationTabViewDataController._();
-    await controller._fetchAvailableTables(date: DateTime.now(), timeSlotStart: 14, timeSlotEnd: 16);
+    await controller._fetchAvailableTables(
+        date: DateTime.now(), timeSlotStart: 14, timeSlotEnd: 16);
     return controller;
   }
 
   final RxList<RestaurantTable> _availableTables = <RestaurantTable>[].obs;
   List<RestaurantTable> get availableTables => _availableTables;
-  final RxList<RestaurantTable> listenableAvailableTables = <RestaurantTable>[].obs;
-
+  final RxList<RestaurantTable> listenableAvailableTables =
+      <RestaurantTable>[].obs;
 
   Future<List<RestaurantTable>> _fetchAvailableTables({
     required DateTime date,
@@ -38,20 +41,20 @@ class AddReservationTabViewDataController extends GetxController {
         timeSlotEnd: timeSlotEnd,
       );
 
-      try{
-        if(dataMap['available_tables'] == null)
-          {
-            throw NullMapKeyException(message: 'NULL Key \'available_tables\'');
-          }
-        _availableTables.value = dataMap['available_tables']!.map<RestaurantTable>(
-            (tableMap) => RestaurantTable.fromMap(tableMap)
-        ).toList();
+      try {
+        if (dataMap['available_tables'] == null) {
+          throw NullMapKeyException(message: 'NULL Key \'available_tables\'');
+        }
+        _availableTables.value = dataMap['available_tables']!
+            .map<RestaurantTable>(
+                (tableMap) => RestaurantTable.fromMap(tableMap))
+            .toList();
 
-        listenableAvailableTables.value = dataMap['available_tables']!.map<RestaurantTable>(
-                (tableMap) => RestaurantTable.fromMap(tableMap)
-        ).toList();
-
-      } on NullMapKeyException catch(e){
+        listenableAvailableTables.value = dataMap['available_tables']!
+            .map<RestaurantTable>(
+                (tableMap) => RestaurantTable.fromMap(tableMap))
+            .toList();
+      } on NullMapKeyException catch (e) {
         //TODO:handle null key
         print(e.toString());
         rethrow;
@@ -69,14 +72,14 @@ class AddReservationTabViewDataController extends GetxController {
     required int timeSlotStart,
     required int timeSlotEnd,
   }) async {
-    try{
-      await _fetchAvailableTables(date: date, timeSlotStart: timeSlotStart, timeSlotEnd: timeSlotEnd);
-    }
-    on NetworkException catch(e){
+    try {
+      await _fetchAvailableTables(
+          date: date, timeSlotStart: timeSlotStart, timeSlotEnd: timeSlotEnd);
+    } on NetworkException catch (e) {
       //TODO:handle errors
-    }
-    on NullMapKeyException catch(e){
+    } on NullMapKeyException catch (e) {
       //TODO:handle errors
     }
   }
+
 }
