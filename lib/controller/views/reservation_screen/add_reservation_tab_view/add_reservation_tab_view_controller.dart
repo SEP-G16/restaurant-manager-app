80ae4bd53a953da802b545f3a5e66337da098183
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-09-25 23:15:09 +0530

    used already initialized date and drop down controllers to merge their functionality to this main controller

--- lib/controller/views/reservation_screen/add_reservation_tab_view/add_reservation_tab_view_controller.dart
+++ lib/controller/views/reservation_screen/add_reservation_tab_view/add_reservation_tab_view_controller.dart
@@ -1,12 +1,20 @@
 import 'package:get/get.dart';
 import 'package:restaurant_manager/controller/data/reservation_screen/add_reservation_tab_view_data_controller.dart';
+import 'package:restaurant_manager/controller/views/reservation_screen/add_reservation_tab_view/add_reservation_tab_view_date_controller.dart';
+import 'package:restaurant_manager/controller/views/reservation_screen/add_reservation_tab_view/add_reservation_tab_view_drop_down_controller.dart';
+import 'package:restaurant_manager/model/reservation.dart';
 
 import '../../../../model/restaurant_table.dart';
 
-class AddReservationTabViewController extends GetxController{
+class AddReservationTabViewController extends GetxController {
   static final AddReservationTabViewController instance = Get.find();
 
   late final AddReservationTabViewDataController _rsdc;
+
+  AddReservationTabViewDateController _dateController =
+      AddReservationTabViewDateController.instance;
+  AddReservationTabViewDropDownController _dropDownController =
+      AddReservationTabViewDropDownController.instance;
 
   final RxList<RestaurantTable> _displayedTables = <RestaurantTable>[].obs;
   List<RestaurantTable> get displayedTables => _displayedTables;
@@ -14,13 +22,12 @@ class AddReservationTabViewController extends GetxController{
   final RxList<RestaurantTable> _selectedTables = <RestaurantTable>[].obs;
   List<RestaurantTable> get selectedTables => _selectedTables;
 
-
   void _initController() {
     _rsdc = AddReservationTabViewDataController.instance;
     _displayedTables.value = _rsdc.availableTables;
   }
 
-  void changeDisplayedTables(int chairCount) {
+  void _changeDisplayedTables(int chairCount) {
     if (chairCount == -1) {
       _displayedTables.value = _rsdc.availableTables;
       return;
@@ -40,8 +47,10 @@ class AddReservationTabViewController extends GetxController{
       }
     } else {
       if (_rsdc.availableTables.any((table) => table.id == id)) {
-        RestaurantTable table =
-            _rsdc.availableTables.where((table) => table.id == id).toList().first;
+        RestaurantTable table = _rsdc.availableTables
+            .where((table) => table.id == id)
+            .toList()
+            .first;
         _selectedTables.add(table);
       } else {
         // throw error and handle it in the UI if necessary
@@ -59,7 +68,7 @@ class AddReservationTabViewController extends GetxController{
     _selectedTables.clear();
   }
 
-  void _onTablesChanged(_){
+  void _onTablesChanged(_) {
     _displayedTables.value = _rsdc.availableTables;
   }
 
@@ -98,6 +107,27 @@ class AddReservationTabViewController extends GetxController{
     // TODO: implement onReady
     super.onReady();
     ever(_rsdc.listenableAvailableTables, _onTablesChanged);
-  }
 
+    ever(_dateController.listenableSelectedDate, (date) async {
+      _dropDownController.setSelectedTimeSlotByDateTime(dateTime: date);
+      Map<String, int> timeSlotMap =
+          _dropDownController.getTimeSlotAsIntegerMap(date);
+      print(timeSlotMap);
+      await _rsdc.fetchAvailableTables(
+          date: date,
+          timeSlotStart: timeSlotMap['start']!,
+          timeSlotEnd: timeSlotMap['end']!);
+    });
+
+    ever(_dropDownController.listenableSelectedTimeSlot, (timeSlot) async {
+      await _rsdc.fetchAvailableTables(
+          date: _dateController.selectedDate,
+          timeSlotStart: int.parse(timeSlot.substring(0, 2)),
+          timeSlotEnd: int.parse(timeSlot.substring(8, 10)));
+    });
+
+    ever(_dropDownController.listenableSelectedCount, (count) {
+      _changeDisplayedTables(count);
+    });
+  }
 }
