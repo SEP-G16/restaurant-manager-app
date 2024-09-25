18c84713956bf75ce60f3deb6c40d4cd3f06cbab
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-09-25 23:19:10 +0530

    modified class with getters and setter and map conversion methods

--- lib/model/inventory_item.dart
+++ lib/model/inventory_item.dart
@@ -1,10 +1,43 @@
 class InventoryItem {
-  final int id;
-  final String name;
-  final bool isInStock;
-  InventoryItem({required this.id, required this.name, required this.isInStock});
+  int _id;
+  String _name;
+  bool _isInStock;
+  InventoryItem({required int id, required String name, required bool isInStock}):
+    _id = id,
+    _name = name,
+    _isInStock = isInStock;
 
-  set id(value) => id = value;
-  set name(value) => name = value;
-  set isInStock(value) => isInStock = value;
+  int get id => _id;
+  String get name => _name;
+  bool get isInStock => _isInStock;
+
+  set id(int id) {
+    _id = id;
+  }
+
+  set name(String name) {
+    _name = name;
+  }
+
+  set isInStock(bool isInStock) {
+    _isInStock = isInStock;
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': _id,
+      'name': _name,
+      'is_in_stock': _isInStock,
+    };
+  }
+
+  factory InventoryItem.fromMap(Map<String, dynamic> map) {
+    return InventoryItem(
+      id: map['id'],
+      name: map['name'],
+      isInStock: map['is_in_stock'],
+    );
+  }
+
+
 }
