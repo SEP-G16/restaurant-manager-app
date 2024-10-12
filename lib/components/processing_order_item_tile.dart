49f74a82e140201b9c3664ed9c13d7819233fdee
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-12 09:02:45 +0530

    modified callback function to pass the error

--- lib/components/processing_order_item_tile.dart
+++ lib/components/processing_order_item_tile.dart
@@ -16,7 +16,7 @@ class ProcessingOrderItemTile extends StatelessWidget {
 
   final OrderItem orderItem;
   final FutureOr<void> Function() onCompletePressed;
-  void Function()? onCompleteErrorCallBack;
+  void Function(Object? e)? onCompleteErrorCallBack;
 
 
   @override
@@ -116,7 +116,7 @@ class ProcessingOrderItemTile extends StatelessWidget {
                     await onCompletePressed();
                   } catch (e) {
                     if (onCompleteErrorCallBack != null) {
-                      onCompleteErrorCallBack!();
+                      onCompleteErrorCallBack!(e);
                     }
                   }
                 },
