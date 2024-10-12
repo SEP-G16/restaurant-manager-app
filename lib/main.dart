63e0a972fed07f8d1596dfd8b3f02a37868f9a54
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-12 08:55:58 +0530

    removed controller initializing

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:restaurant_manager/constants/colour_constants.dart';
-import 'package:restaurant_manager/views/kitchen/inventory_management_screen.dart';
-import 'package:restaurant_manager/controller/views/inventory_controller.dart';
 import 'package:restaurant_manager/views/login_screen.dart';
 
 void main() {
-  // Initialize GetX controllers
-  Get.put(InventoryController());
-
   runApp(const RestaurantManager());
 }
 
