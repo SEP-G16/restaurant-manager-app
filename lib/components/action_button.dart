aad2bd47d7680089e76870ae4a53b46edea2c716
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-12 08:36:41 +0530

    added button disable mode

--- lib/components/action_button.dart
+++ lib/components/action_button.dart
@@ -18,6 +18,7 @@ class ActionButton extends StatelessWidget {
     this.fontWeight,
     this.outlineModeHoverColour,
     this.outlineModeTextColour,
+    this.isDisabled = false,
   });
 
   final String btnText;
@@ -33,42 +34,84 @@ class ActionButton extends StatelessWidget {
   FontWeight? fontWeight;
   Color? outlineModeHoverColour;
   Color? outlineModeTextColour;
-
+  bool isDisabled;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(color: outlineMode ? borderColour ?? ColourConstants.gamboge : Colors.transparent, width: borderWidth, strokeAlign: BorderSide.strokeAlignOutside),
+        border: Border.all(
+            color: isDisabled
+                ? Colors.black.withOpacity(0.6)
+                : outlineMode
+                    ? borderColour ?? ColourConstants.gamboge
+                    : Colors.transparent,
+            width: borderWidth,
+            strokeAlign: BorderSide.strokeAlignOutside),
         borderRadius: BorderRadius.circular(10.0),
       ),
       height: height ?? 60,
       width: width ?? 150,
-      child: Material(
-        color:
-        outlineMode ? Colors.transparent :
-        btnColor ?? ColourConstants.gamboge,
-        borderRadius: BorderRadius.circular(10.0),
-        child: InkWell(
-          onTap: onTap,
-          overlayColor: WidgetStateProperty.resolveWith(
-                (_) => outlineMode ? outlineModeHoverColour ?? ColourConstants.gamboge.withOpacity(0.1) :ColourConstants.chineseBlack.withOpacity(
-              0.1,
+      child: Stack(
+        children: [
+          Material(
+            color: outlineMode
+                ? Colors.transparent
+                : btnColor ?? ColourConstants.gamboge,
+            borderRadius: BorderRadius.circular(10.0),
+            child: isDisabled
+                ? Center(
+                    child: Text(
+                      btnText,
+                      textAlign: TextAlign.center,
+                      style: TextConstants.subTextStyle(
+                        color: isDisabled
+                            ? Colors.black.withOpacity(0.6)
+                            : outlineMode
+                                ? outlineModeTextColour ??
+                                    borderColour ??
+                                    ColourConstants.gamboge
+                                : textColor ?? ColourConstants.chineseBlack,
+                        fontSize: fontSize ?? 22,
+                        fontWeight: fontWeight ?? FontWeight.w600,
+                      ),
+                    ),
+                  )
+                : InkWell(
+                    onTap: onTap,
+                    overlayColor: WidgetStateProperty.resolveWith(
+                      (_) => outlineMode
+                          ? outlineModeHoverColour ??
+                              ColourConstants.gamboge.withOpacity(0.1)
+                          : ColourConstants.chineseBlack.withOpacity(
+                              0.1,
+                            ),
+                    ),
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Center(
+                      child: Text(
+                        btnText,
+                        textAlign: TextAlign.center,
+                        style: TextConstants.subTextStyle(
+                          color: outlineMode
+                              ? outlineModeTextColour ??
+                                  borderColour ??
+                                  ColourConstants.gamboge
+                              : textColor ?? ColourConstants.chineseBlack,
+                          fontSize: fontSize ?? 22,
+                          fontWeight: fontWeight ?? FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+          ),
+          Visibility(
+            visible: isDisabled,
+            child: Container(
+              color: Colors.black54.withOpacity(0.1),
             ),
           ),
-          borderRadius: BorderRadius.circular(10.0),
-          child: Center(
-            child: Text(
-              btnText,
-              textAlign: TextAlign.center,
-              style: TextConstants.subTextStyle(
-                color: outlineMode ? outlineModeTextColour ?? borderColour ?? ColourConstants.gamboge : textColor ?? ColourConstants.chineseBlack,
-                fontSize: fontSize ?? 22,
-                fontWeight: fontWeight ?? FontWeight.w600,
-              ),
-            ),
-          ),
-        ),
+        ],
       ),
     );
   }
