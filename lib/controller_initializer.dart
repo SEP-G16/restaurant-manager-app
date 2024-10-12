7f144be4932956b7abcc43ebb92f1c11ae555e13
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-12 08:36:59 +0530

    new state controller initialized

--- lib/controller_initializer.dart
+++ lib/controller_initializer.dart
@@ -1,12 +1,18 @@
 import 'package:get/get.dart';
+import 'package:restaurant_manager/controller/data/order_screen/order_data_controller.dart';
 import 'package:restaurant_manager/controller/data/reservation_screen/add_reservation_tab_view_data_controller.dart';
 import 'package:restaurant_manager/controller/data/reservation_screen/view_reservations_tab_view_data_controller.dart';
+import 'package:restaurant_manager/controller/network/inventory_network_controller.dart';
 import 'package:restaurant_manager/controller/network/reservation_network_controller.dart';
 import 'package:restaurant_manager/controller/network/web-socket-controller.dart';
+import 'package:restaurant_manager/controller/views/orders_screen/order_screen_main_state_controller.dart';
 import 'package:restaurant_manager/controller/views/reservation_screen/add_reservation_tab_view/add_reservation_tab_view_controller.dart';
 import 'package:restaurant_manager/controller/views/reservation_screen/add_reservation_tab_view/add_reservation_tab_view_date_controller.dart';
 import 'package:restaurant_manager/controller/views/reservation_screen/add_reservation_tab_view/add_reservation_tab_view_drop_down_controller.dart';
+import 'package:restaurant_manager/controller/views/view_inventory_screen/inventory_screen_state_controller.dart';
 import 'package:restaurant_manager/controller/views/welcome_screen_controller.dart';
+
+import 'controller/data/inventory_screen/inventory_data_controller.dart';
 
 class ControllerInitializer{
   static bool _initialized = false;
@@ -27,9 +33,16 @@ class ControllerInitializer{
       Get.put(AddReservationTabViewController());
 
       await Get.putAsync(() => ViewReservationsTabViewDataController.create());
+
+      await Get.putAsync(() => OrderDataController.create());
+      Get.put(OrderScreenMainStateController());
+
+      Get.put(InventoryNetworkController());
+      await Get.putAsync(() => InventoryDataController.create());
+      Get.put(InventoryScreenStateController());
       _initialized = true;
     }catch(e){
-      print('Error occurred while initialising controllers!');
+      print('Error occurred while initialising controllers! : $e');
       _initialized = false;
       rethrow;
     }
