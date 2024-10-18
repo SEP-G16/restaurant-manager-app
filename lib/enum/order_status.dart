d7503dfe7162118051d4dc9ada1e18e9b250ac0f
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-18 21:44:16 +0530

    added function to get enum from name

--- lib/enum/order_status.dart
+++ lib/enum/order_status.dart
@@ -1,3 +1,20 @@
 enum OrderStatus{
-  Pending, Processing, Complete, Cancelled
+  Pending, Processing, Complete, Cancelled, Pending_Payment;
+
+  static OrderStatus fromString(String status){
+    switch(status){
+      case 'Pending':
+        return OrderStatus.Pending;
+      case 'Processing':
+        return OrderStatus.Processing;
+      case 'Complete':
+        return OrderStatus.Complete;
+      case 'Cancelled':
+        return OrderStatus.Cancelled;
+      case 'Pending_Payment':
+        return OrderStatus.Pending_Payment;
+      default:
+        return OrderStatus.Pending;
+    }
+  }
 }
