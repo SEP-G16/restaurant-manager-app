865e3782a8ec2bd024960cbef27090941a247c9b
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-18 21:32:39 +0530

    used role-based access control

--- lib/views/loading_screen.dart
+++ lib/views/loading_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:restaurant_manager/constants/text_constants.dart';
+import 'package:restaurant_manager/controller/data/auth_controller.dart';
 import 'package:restaurant_manager/controller_initializer.dart';
 import 'package:restaurant_manager/views/front_desk/reservation_screen.dart';
 import 'package:restaurant_manager/views/kitchen_staff/order/order_main_screen.dart';
-import 'package:restaurant_manager/views/welcome_screen.dart';
+import 'package:restaurant_manager/views/login_screen.dart';
+
+import '../enum/role.dart';
 
 class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key});
@@ -23,35 +26,82 @@ class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: initControllers(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
+      future: initControllers(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          if (AuthController.instance.hasRole([Role.ROLE_ADMIN])) {
             return ReservationScreen();
-          } else if (snapshot.hasError) {
-            return Scaffold(
-              body: SafeArea(
-                child: Container(
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'An unexpected error occurred!',
-                    style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.redAccent,
+          }
+
+          if (AuthController.instance.hasRole([Role.ROLE_CHEF])) {
+            return OrderMainScreen();
+          }
+
+          if (AuthController.instance.hasRole([
+            Role.ROLE_RESTAURANT_MANAGER,
+          ])) {
+            return ReservationScreen();
+          }
+
+          if (AuthController.instance.hasRole([Role.ROLE_FRONT_DESK])) {
+            return ReservationScreen();
+          }
+
+          Future<String> wait() async {
+            await Future.delayed(const Duration(seconds: 5));
+            return '';
+          }
+
+          return FutureBuilder(
+              future: wait(),
+              builder: (builder, snapshot) {
+                if (snapshot.hasData) {
+                  AuthController.instance.logout();
+                  return LoginScreen();
+                } else {
+                  return Scaffold(
+                    body: SafeArea(
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'You are unauthorized to access this app!\nYou\'ll be redirected to the Login Page',
+                          style: TextConstants.mainTextStyle(),
+                        ),
+                      ),
                     ),
+                  );
+                }
+              });
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            body: SafeArea(
+              child: Container(
+                alignment: Alignment.center,
+                child: const Text(
+                  'An unexpected error occurred!',
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.redAccent,
                   ),
                 ),
               ),
-            );
-          } else {
-            return Scaffold(
-              body: SafeArea(
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Center(child: CircularProgressIndicator(color: Colors.amber,),)
+            ),
+          );
+        } else {
+          return Scaffold(
+            body: SafeArea(
+              child: Container(
+                alignment: Alignment.center,
+                child: Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.amber,
+                  ),
                 ),
               ),
-            );
-          }
-        });
+            ),
+          );
+        }
+      },
+    );
   }
 }
