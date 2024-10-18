1f24565756f3a1387976777aa78398709198b3a9
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-18 21:39:19 +0530

    used new table model

--- lib/model/order.dart
+++ lib/model/order.dart
@@ -1,60 +1,68 @@
+import 'package:restaurant_manager/model/order_table.dart';
+
 import 'order_item.dart';
 import '../enum/order_status.dart';
 
 class Order {
   final int id;
-  final int tableNo;
+  final OrderTable table;
   final List<OrderItem> orderItems;
-  final double totalAmount;
-  final OrderStatus status;
+  OrderStatus _status = OrderStatus.Pending;
   final DateTime orderTime;
+  String sessionId;
+
+  OrderStatus get status => _status;
+  set status(OrderStatus value) => _status = value;
+
+  double get totalAmount =>  orderItems.fold(0, (previousValue, element) => previousValue + element.totalAmount);
 
   Order({
     required this.id,
-    required this.tableNo,
+    required this.table,
     required this.orderItems,
-    required this.totalAmount,
-    required this.status,
+    required OrderStatus status,
     required this.orderTime,
-  });
+    required this.sessionId,
+  }){
+    _status = status;
+  }
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'tableNo': tableNo,
+      'table': table.toMap(),
       'orderItems': orderItems.map((item) => item.toMap()).toList(),
-      'totalAmount': totalAmount,
-      'status': status.toString().split('.').last,
+      'status': _status.toString().split('.').last,
       'orderTime': orderTime.toIso8601String(),
+      'sessionId' : sessionId,
     };
   }
 
   factory Order.fromMap(Map<String, dynamic> map) {
     return Order(
       id: map['id'],
-      tableNo: map['tableNo'],
+      table: OrderTable.fromMap(map['table']!),
       orderItems: List<OrderItem>.from(map['orderItems']?.map((item) => OrderItem.fromMap(item))),
-      totalAmount: map['totalAmount'],
-      status: OrderStatus.values.where((stat) => stat.toString().toLowerCase().split('.').last == map['status'].toString().toLowerCase()).toList().first,
-      orderTime: DateTime.parse(map['orderTime']),
+      status: OrderStatus.fromString(map['status']),
+      orderTime: DateTime.parse(map['orderTime']), sessionId: map['sessionId'],
     );
   }
 
   Order copyWith({
     int? id,
-    int? tableNo,
+    OrderTable? table,
     List<OrderItem>? orderItems,
-    double? totalAmount,
     OrderStatus? status,
     DateTime? orderTime,
+    String? sessionId
   }) {
     return Order(
       id: id ?? this.id,
-      tableNo: tableNo ?? this.tableNo,
+      table: table ?? this.table,
       orderItems: orderItems ?? this.orderItems,
-      totalAmount: totalAmount ?? this.totalAmount,
-      status: status ?? this.status,
+      status: status ?? _status,
       orderTime: orderTime ?? this.orderTime,
+      sessionId: this.sessionId
     );
   }
 }
