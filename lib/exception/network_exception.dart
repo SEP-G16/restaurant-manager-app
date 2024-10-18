f688979e6d7090154a88183be4841f756cf35885
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-18 21:39:03 +0530

    modified not to use named args

--- lib/exception/network_exception.dart
+++ lib/exception/network_exception.dart
@@ -1,7 +1,7 @@
 import 'package:restaurant_manager/exception/base_exception.dart';
 
 class NetworkException extends BaseException{
-  NetworkException({String? message}) : super(message: message);
+  NetworkException(String? message) : super(message: message);
 
   @override
   String toString() => message ?? super.toString();
