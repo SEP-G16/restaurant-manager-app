981313683c66dfade5869a004f49f5e9a93f1919
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-18 21:36:10 +0530

    reduced code duplication and added authentication

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -6,13 +6,66 @@ import 'package:restaurant_manager/constants/colour_constants.dart';
 import 'package:restaurant_manager/constants/svg_constants.dart';
 import 'package:restaurant_manager/constants/text_constants.dart';
 import 'package:get/get.dart';
-import 'package:restaurant_manager/views/loading_screen.dart';
+import 'package:restaurant_manager/controller/data/auth_controller.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    String? email;
+    String? password;
+
+    Widget _buildLoginForm() {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Login to Your Account',
+            style: TextConstants.mainTextStyle(fontSize: 40),
+          ),
+          SizedBox(height: 50.0),
+          InputField(
+            labelText: 'Email',
+            onChanged: (value) {
+              email = value;
+            },
+          ),
+          SizedBox(height: 20.0),
+          InputField(
+            labelText: 'Password',
+            onChanged: (value) {
+              password = value;
+            },
+          ),
+          SizedBox(height: 20.0),
+          ActionButton(
+            btnText: 'Login',
+            onTap: () async {
+              if (email == null) {
+                Get.snackbar('Error', 'Please enter your email',
+                    snackPosition: SnackPosition.BOTTOM);
+                return;
+              }
+              if (password == null) {
+                Get.snackbar('Error', 'Please enter your password',
+                    snackPosition: SnackPosition.BOTTOM);
+                return;
+              }
+              try {
+                await AuthController.instance
+                    .login(email: email!, password: password!);
+              } catch (e) {
+                print(e);
+                Get.snackbar('Error', e.toString(),
+                    snackPosition: SnackPosition.BOTTOM);
+              }
+            },
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       backgroundColor: ColourConstants.white,
       body: GestureDetector(
@@ -22,81 +75,26 @@ class LoginScreen extends StatelessWidget {
             alignment: Alignment.center,
             child: OrientationBuilder(
               builder: (context, orientation) {
-                print(orientation);
-                if (orientation == Orientation.portrait) {
-                  return SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SvgPicture.string(SvgConstants.loginCoverImage),
-                        SizedBox(
-                          height: 50.0,
-                        ),
-                        Text(
-                          'Login to Your Account',
-                          style: TextConstants.mainTextStyle(fontSize: 40),
-                        ),
-                        SizedBox(
-                          height: 50.0,
-                        ),
-                        InputField(
-                          labelText: 'Email',
-                          onChanged: (email) {},
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        InputField(
-                          labelText: 'Password',
-                          onChanged: (password) {},
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        ActionButton(btnText: 'Login', onTap: () {
-                          Get.to(() => LoadingScreen());
-                        }),
-                      ],
-                    ),
-                  );
-                } else {
-                  return SingleChildScrollView(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SvgPicture.string(SvgConstants.loginCoverImage),
-                        Column(
+                final isPortrait = orientation == Orientation.portrait;
+                return SingleChildScrollView(
+                  child: isPortrait
+                      ? Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Text(
-                              'Login to Your Account',
-                              style: TextConstants.mainTextStyle(fontSize: 40),
-                            ),
-                            SizedBox(
-                              height: 50.0,
-                            ),
-                            InputField(
-                              labelText: 'Email',
-                              onChanged: (email) {},
-                            ),
-                            SizedBox(
-                              height: 20.0,
-                            ),
-                            InputField(
-                              labelText: 'Password',
-                              onChanged: (password) {},
-                            ),
-                            SizedBox(
-                              height: 20.0,
-                            ),
-                            ActionButton(btnText: 'Login', onTap: () {
-                              Get.to(() => LoadingScreen());
-                            }),
+                            SvgPicture.string(SvgConstants.loginCoverImage),
+                            SizedBox(height: 50.0),
+                            _buildLoginForm(),
+                          ],
+                        )
+                      : Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.string(SvgConstants.loginCoverImage),
+                            SizedBox(width: 50.0),
+                            _buildLoginForm(),
                           ],
                         ),
-                      ],
-                    ),
-                  );
-                }
+                );
               },
             ),
           ),
