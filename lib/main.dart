cc5fa31e21b82131ba2e536321c6158d39340c1e
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-18 21:37:32 +0530

    used welcome screen, secure storage, auth controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:restaurant_manager/constants/colour_constants.dart';
-import 'package:restaurant_manager/views/login_screen.dart';
+import 'package:restaurant_manager/controller/data/auth_controller.dart';
+import 'package:restaurant_manager/controller/network/auth_network_controller.dart';
+import 'package:restaurant_manager/views/welcome_screen.dart';
 
-void main() {
+import 'controller/data/secure_storage_controller.dart';
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+void main() async {
+  HttpOverrides.global = MyHttpOverrides();
+  Get.put(SecureStorageController());
+  Get.put(AuthNetworkController());
   runApp(const RestaurantManager());
+  await Get.putAsync(() => AuthController.create());
 }
 
 class RestaurantManager extends StatelessWidget {
@@ -24,7 +43,7 @@ class RestaurantManager extends StatelessWidget {
         canvasColor: ColourConstants.white,
         colorScheme: ColorScheme.light(primary: ColourConstants.gamboge),
       ),
-      home: LoginScreen(),
+      home: WelcomeScreen(),
     );
   }
 }
