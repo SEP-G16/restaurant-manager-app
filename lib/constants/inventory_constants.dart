10244a649cdbce8289fa8f8b4a25c771cc76c4f3
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-18 21:31:02 +0530

    modified due to model changes.

--- lib/constants/inventory_constants.dart
+++ lib/constants/inventory_constants.dart
@@ -1,12 +1,13 @@
+import 'package:restaurant_manager/enum/menu_item_status.dart';
 import 'package:restaurant_manager/model/inventory_item.dart';
 
 class InventoryConstants {
   InventoryConstants._();
 
   static final List<InventoryItem> inventory = [
-    InventoryItem(id : 1, name: 'Chicken Bun', isInStock: true),
-    InventoryItem(id : 2, name: 'Chicken Dumpling', isInStock: true),
-    InventoryItem(id : 3, name: 'Soup', isInStock: false),
-    InventoryItem(id : 4, name: 'Cake', isInStock: false),
+    InventoryItem(id : 1, name: 'Chicken Bun', status: MenuItemStatus.InStock),
+    InventoryItem(id : 2, name: 'Chicken Dumpling', status: MenuItemStatus.InStock),
+    InventoryItem(id : 3, name: 'Soup', status: MenuItemStatus.OutOfStock),
+    InventoryItem(id : 4, name: 'Cake', status: MenuItemStatus.OutOfStock),
   ];
 }
